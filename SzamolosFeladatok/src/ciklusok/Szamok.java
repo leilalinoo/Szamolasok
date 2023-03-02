880e99bcbb5494a6e6d891ac31c16e0a2bcbbe94
leilalino <aleila0921@gmail.com>
2023-03-02 16:02:24 +0100

    harom ciklus

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -4,5 +4,15 @@ public class Szamok {
 
     public static void main(String[] args) {
 
+
+        for (int j = 2; j < 3; j++) {
+            for (int y = 2; y < 3; y++) {
+                for (int i = 2; i <= 4; i++) {
+                    System.out.println(j + "" + y + "" + i);
+                }
+            }
+
+        }
+
     }
 }
