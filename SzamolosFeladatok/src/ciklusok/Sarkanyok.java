ff685e69dcd43073de96fcc4ab1b9ae4337a525c
leilalino <aleila0921@gmail.com>
2023-03-02 16:39:51 +0100

    kesz feladat - ismetles/nelkuli

---
+++ SzamolosFeladatok/src/ciklusok/Sarkanyok.java
@@ -0,0 +1,8 @@
+package ciklusok;
+
+public class Sarkanyok {
+
+    public static void main(String[] args) {
+
+    }
+}
--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,18 +1,36 @@
 package ciklusok;
 
+import java.util.Scanner;
+
 public class Szamok {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in, "latin2");
+        System.out.println("Ismétléssel, vagy ismétlés nélül szeretné látni a számokat? (ism/nél");
+        String kerdes = sc.next();
 
-        for (int j = 2; j < 3; j++) {
-            for (int y = 2; y < 3; y++) {
+        int ismdb = 0;
+        int nelkuldb = 0;
+
+        for (int j = 2; j <= 4; j++) {
+            for (int y = 2; y <= 4; y++) {
                 for (int i = 2; i <= 4; i++) {
-                    System.out.println(j + "" + y + "" + i);
+                    if (kerdes.equals("ism")) {
+                        System.out.println(j + "" + y + "" + i);
+                        ismdb++;
+                    } else if (i != y && i != j && j != y) {
+                        System.out.println("" + j + y + i);
+                        nelkuldb++;
+                    }
                 }
             }
-
         }
-
+        if (kerdes.equals("ism")) {
+            System.out.println("Ismételt dbszám: " + ismdb);
+        }
+        else{
+            System.out.println("Ismétlés nélküli dbszám: "+nelkuldb);
+        }
     }
 }
